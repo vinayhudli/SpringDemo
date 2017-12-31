e7dcf1d5c57b583298ba779087b23d69eaa658f1
vinay_deshpande <vinay.deshpande@rakuten.com>
2017-12-31 16:40:14 +0530

    initial commit

---
+++ src/main/java/com/spring/app/configuration/AppConfiguration.java
@@ -0,0 +1,10 @@
+package com.spring.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="")
+public class AppConfiguration {
+
+}
---
+++ src/main/java/com/spring/app/entities/Game.java
@@ -0,0 +1,5 @@
+package com.spring.app.entities;
+
+public interface Game {
+
+}
