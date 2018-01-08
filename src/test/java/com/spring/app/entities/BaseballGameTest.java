1240c02398b62b400a8219a9af8b2a440e797b13
vinay_deshpande <vinay.deshpande@rakuten.com>
2018-01-08 08:23:48 +0530

    adding test case

--- src/test/java/com/spring/app/entities/BaseballGameTest.java
+++ src/test/java/com/spring/app/entities/BaseballGameTest.java
@@ -1,5 +1,30 @@
 package com.spring.app.entities;
 
-public class BaseballGameTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.spring.app.configuration.AppConfiguration;
+
+public class BaseballGameTest {
+	
+	private Game game ;
+	
+	@Before
+	public void setUp(){
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class) ;
+		game = context.getBean("getGame", Game.class) ;
+	}
+	
+	@Test
+	public void testPlayGame(){
+		String homeTeam = game.getHomeTeam().getName() ;
+		String awayTeam = game.getAwayTeam().getName() ;
+		
+		String winningTeam = game.playGame() ;
+		
+		Assert.assertTrue(winningTeam.equals(awayTeam) || winningTeam.equals(homeTeam));
+	}
 }
